f01a778588ef76001b94962323030eb46a07805f
astranono <90936000+astranono@users.noreply.github.com>
2026-05-06 11:23:38 -0400

    fisrt coomit

--- WikiGame.java
+++ WikiGame.java
@@ -10,7 +10,7 @@ public class WikiGame {
     }
 
     public WikiGame() {
-
+//test
         String startLink = "";  // beginning link, where the program will start
         String endLink = "";    // ending link, where the program is trying to get to
         maxDepth = 1;           // start this at 1 or 2, and if you get it going fast, increase
